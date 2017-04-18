877f5a46356ee7cbb3def7a34314473f6646defe
NicoHova <nicohova.95@hotmail.com>
2017-04-18 14:22:06 -0300

    menues

---
+++ ASIC.m
@@ -0,0 +1,52 @@
+function menuPrincipal
+	printf("Elija una opción.\n\n")
+	printf("1) Obtener todas las características de la función.\n")
+  	printf("2) Seleccionar alguna característica en particular.\n")
+endfunction
+
+function caracteristicas
+	printf("Elija una opción.\n\n")
+  	printf("1) Obtener la expresión de la función transferencia.\n")
+  	printf("2) Indicar Polos.\n")
+  	printf("3) Indicar Ceros.\n")
+  	printf("4) Marcar ganancia de la función.\n")
+  	printf("5) Obtener expresión con sus polos, ceros y ganancia.\n")
+  	printf("6) Mostrar gráficamente la distribución de polos y ceros.\n")
+  	printf("7) Indicar estabilidad del sistema.\n")
+endfunction
+
+function menuFinalizacion
+	printf("1) Ingresar otra función.\n")
+	printf("2) Finalizar.\n")
+endfunction
+
+function polos(functf)
+	
+endfunction
+
+function ceros(functf)
+	
+endfunction
+
+function ganancia(functf)
+	
+endfunction
+
+function polosCerosGanancia(functf)
+	
+endfunction
+
+function estabilidad(functf)
+	
+endfunction
+
+function distribuciónPolosyCeros(functf)
+	
+endfunction
+
+//COMIENZO DEL PROGRAMA--------------------------------------------------------------------
+
+func = input ("Ingrese una función de transferencia\n");
+
+
+//FIN DEL PROGRAMA--------------------------------------------------------------------
